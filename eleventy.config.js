b9675e232f46641bc3aec50819ed93ee456aca8e
fpigeonjr <frank.pigeonjr@gmail.com>
2024-05-11 18:18:30 -0500

    converts to TypeScript

--- eleventy.config.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return {
-    dir: {
-      input: "site",
-      output: "_site",
-    },
-  }
-}
---
+++ site/index.11ty.ts
@@ -0,0 +1,3 @@
+export function render(): string {
+  return "<h1>Hello World</h1>"
+}
