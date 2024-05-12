2df030ef92070bd14fc7df01fa529822ed09d613
fpigeonjr <frank.pigeonjr@gmail.com>
2024-05-11 21:17:45 -0500

    adds testing library and happy-dom

--- site/index.test.tsx
+++ site/index.test.tsx
@@ -1,10 +1,11 @@
 import { expect, test } from "vitest"
 import { Index } from "./index.11ty"
 import { renderToString } from "jsx-async-runtime"
+import { screen } from "@testing-library/dom"
 
 test("Render Index", async () => {
   const result = <Index />
-  const rendered = await renderToString(result)
+  document.body.innerHTML = await renderToString(result)
 
-  expect(rendered).toEqual("<h1>Hello TSX</h1>")
+  expect(screen.getByText("Hello TSX")).to.exist
 })
